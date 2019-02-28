e9fe627f649703f90b86888dfb19fafdc234d38c
Aaron Waguespack <Aaron_Waguespack@hotmail.com>
2019-02-28 16:37:51 -0700

    project link updates

--- src/components/projectlist.js
+++ src/components/projectlist.js
@@ -3,7 +3,7 @@ export const projects = [
 id: 1,
 image:"assets/facerec.jpg",
 name: "Face Recognition",
-desc: "Project still to be deployed - App that allows you to create and account using a self learning API it locates faces on a picture and puts a box around it. Built with React for the front end, Node.js and Express.js for the back end, Postgresql for the database created on Sublime3 and deployed on heroku.",
+desc: "Project still to be deployed - App that allows you to create and account using a self learning Clarifai API it locates faces on a picture and puts a box around it. Built with HTML5, CCS3, Javascript, React for the front end, Node.js, NPM, Express.js for the back end. User names and (hashed) password are stored using Postgresql for the database. It was created on Sublime3 and deployed on Heroku.",
 link: "https://github.com/Aaron-Waguespack/robofriends",
 gitlink: "https://github.com/Aaron-Waguespack/robofriends"
 },
@@ -11,7 +11,7 @@ gitlink: "https://github.com/Aaron-Waguespack/robofriends"
 id: 2,
 image:"assets/yelpcamp.jpg",
 name: "Yelp Camp",
-desc: "YelpCamp is a yelp style website for campgrounds.Once you create an account you can add and edit campsites, as well as add comments on other campsites. It uses HTML5, CCS3, Javascript, Bootstrap4, jQuery, Node.js, NPM, Express.js, REST, Passport.js, MongoDB. Develpoed on Cloud9 IDE and hosted on Heroku and MongoLab.",
+desc: "YelpCamp is a yelp style website for campgrounds.Once you create an account you can add and edit campsites, as well as add comments on other campsites. It uses HTML5, CCS3, Javascript, Bootstrap4, jQuery on the front end, Node.js, NPM, Express.js, REST, Passport.js, MongoDB. Develpoed on Cloud9 IDE and hosted on Heroku and MongoLab.",
 link: "https://yelpcamp-aw.herokuapp.com/",
 gitlink: "https://github.com/Aaron-Waguespack/YelpCamp"
 },
@@ -19,7 +19,7 @@ gitlink: "https://github.com/Aaron-Waguespack/YelpCamp"
 id: 3,
 image:"assets/RoboFriends.jpg",
 name: "RoboFriends",
-desc: "Using React this app dynamically creates cards for each robot from a url list. From there the search box removes unrelated cards on the click of each letter. Created with React, React-Dom and tachyons.",
+desc: "Using React this app dynamically creates cards for each robot from a url list. From there the search box removes unrelated cards on the click of each letter. Created with HTML5, CCS3, Javascript, React, React-Dom and tachyons.",
 link: "https://robofriends-aw.herokuapp.com",
 gitlink: "https://github.com/Aaron-Waguespack/robofriends"
 },
@@ -27,7 +27,7 @@ gitlink: "https://github.com/Aaron-Waguespack/robofriends"
 id: 4,
 image:"assets/patatapclone.jpg",
 name: "Patatap Clone",
-desc: "Music creation app with sound and animations on keypress. Using the jQuery, Howler JS and Paper JS libraries.",
+desc: "Music creation app with sound and animations on keypress. Created with HTML5, CCS3, Javascript, using the jQuery, Howler JS and Paper JS libraries.",
 link: "https://aaron-waguespack.github.io/patatapclone01/",
 gitlink: "https://github.com/Aaron-Waguespack/patatapclone01"
 },
@@ -35,7 +35,7 @@ gitlink: "https://github.com/Aaron-Waguespack/patatapclone01"
 id: 5,
 image:"assets/rgbcolorgame.jpg",
 name: "RGB Color Game",
-desc: "Project to practice RGB color recognition and DOM manipulation. Created primarily in Javascript.",
+desc: "Project to practice RGB color recognition and DOM manipulation. Created with HTML5, CCS3 and Javascript.",
 link: "https://rgbcolorgame-aw.herokuapp.com/",
 gitlink: "https://github.com/Aaron-Waguespack/rgbColorGame"
 },
@@ -43,7 +43,7 @@ gitlink: "https://github.com/Aaron-Waguespack/rgbColorGame"
 id: 6,
 image:"assets/backgroundgen.jpg",
 name: "Background Generator",
-desc: "Project that allows you to pick colors from the color wheel then have them automatically applied to the background gradients. It also provides you the codes for the colors and a section to copy the code to apply it to other projects. Includes a randomize button that picks random colors and applies them to the background. Created in Javascript.",
+desc: "Project that allows you to pick colors from the color wheel then have them automatically applied to the background gradients. It also provides you the codes for the colors and a section to copy the code to apply it to other projects. Includes a randomize button that picks random colors and applies them to the background. Created with HTML5, CCS3 and Javascript.",
 link: "https://backgroundgen.herokuapp.com/",
 gitlink: "https://github.com/Aaron-Waguespack/backgroundgen"
 },
@@ -51,32 +51,32 @@ gitlink: "https://github.com/Aaron-Waguespack/backgroundgen"
 id: 7,
 image:"assets/wedding.jpg",
 name: "Wedding Photos",
-desc: "Created a photo gallery which dynamically creates and expands the list of photos based on the total amount of photos in the directory. Contains onclicks, mouseover effects and a thumbnail gallery	whenever a photo is expanded. Uses Bootstrap 3 & Lightbox",
-link: "https://robofriends-aw.herokuapp.com",
-gitlink: "project 3 git link"
+desc: "Created a photo gallery which dynamically creates and expands the list of photos based on the total amount of photos in the directory. Contains onclicks, mouseover effects and a thumbnail gallery	whenever a photo is expanded. Created with HTML5, CCS3, Javascript, Bootstrap 3 & Lightbox",
+link: "https://aaron-waguespack.github.io/weddingpictures/",
+gitlink: "https://github.com/Aaron-Waguespack/weddingpictures"
 },
 {
 id: 8,
 image:"assets/todolist.jpg",
 name: "To Do List",
-desc: "A small to do list app created in jQuery that allows you add new items, remove items, cross off items. Also uses css gradiantes.",
-link: "https://patatapclone-aw.herokuapp.com/",
-gitlink: "project 4 git link"
+desc: "A small to do list app that allows you add new items, remove items, cross off items. Created with HTML5, CCS3, Javascript and jQuery.",
+link: " https://aaron-waguespack.github.io/todolist//",
+gitlink: "https://github.com/Aaron-Waguespack/todolist"
 },
 {
 id: 9,
 image:"assets/datingapp.jpg",
 name: "Dating App",
-desc: "Landing Page with navbar, buttons, which uses Bootstrap 3 and Google Fonts",
-link: "https://robofriends-aw.herokuapp.com",
-gitlink: "project 3 git link"
+desc: "Landing Page with navbar, buttons. Created with HTML5, CCS3, Javascript Bootstrap 3 and Google Fonts",
+link: "https://aaron-waguespack.github.io/datingapp/",
+gitlink: "https://github.com/Aaron-Waguespack/datingapp"
 },
 {
 id: 10,
 image:"assets/museumofcandy.jpg",
 name: "Museum of Candy",
-desc: "Responsive landing page made with Bootstrap 4 customized not to look like Bootstrap.",
-link: "https://patatapclone-aw.herokuapp.com/",
-gitlink: "project 4 git link"
+desc: "Responsive landing page made not to look like Bootstrap.Created with HTML5, CCS3, Javascript Bootstrap 4",
+link: "https://aaron-waguespack.github.io/museumofcandy/",
+gitlink: "https://github.com/Aaron-Waguespack/museumofcandy"
 }
 ]
