dbcbf4b87e6930e7f04e408ec5dfad8534598186
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2020-08-24 09:27:20 -0600

    updated gamepicker info

--- src/components/projectlist.js
+++ src/components/projectlist.js
@@ -19,7 +19,7 @@ export const projects = [
     id: 3,
     image:"assets/mvp1.png",
     name: "Game Picker",
-    desc: "This project was to create a steam game picker. Using the Steam games API, download a user's game library. Then match that information with game information. Display each game in a card that is filterable by genre. Created using React, Node, Express, Bootstrap",
+    desc: "Using the Steam games API, download a user's game library. Matches librify information with information from each game. Display each game in a card that is filterable by genre. Created using React, Node, Express, Bootstrap, Heroku",
     link: "https://gamepicker-01.herokuapp.com/",
     gitlink: "https://github.com/Aaron-Waguespack/game_picker"
 },
