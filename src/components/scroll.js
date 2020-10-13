0000f8bd7ebbc2f658d7a0e4bb64dce63159c239
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2020-10-13 15:59:09 -0600

    testing updates

---
+++ src/components/scroll.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+  state = {
+    opacity: '1'
+  }
+
+  componentDidMount() {
+    if (typeof window !== "undefined") {
+      window.onscroll = () => {
+        let currentScrollPos = window.pageYOffset;
+        let maxScroll = document.body.scrollHeight - window.innerHeight;
+        // console.log(maxScroll)
+        if (currentScrollPos > 0 && currentScrollPos < maxScroll) {
+          this.setState({ opacity: "0" })
+          // console.log(currentScrollPos)
+        } else {
+          this.setState({ opacity: "1" })
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+        <div className="your-class" style={{ opacity: `${this.state.opacity}`}} >
+         {/* additonal content */} Hello World
+        </div>
+    )
+  }
+}
+
+
+export default Index
