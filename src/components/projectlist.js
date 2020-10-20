197d3c45af85efea16e948c865f8bf7d95be7667
Aaron Waguespack <Aaron.Paul.Waguespack@gmail.com>
2020-10-20 08:37:35 -0600

    added polebarn

--- src/components/projectlist.js
+++ src/components/projectlist.js
@@ -1,6 +1,14 @@
 export const projects = [
 {
     id: 1,
+    image:"assets/polebarn.png",
+    name: "Polebarn",
+    desc: "Polebarn is an working single page website mockup for a client. It mobile moblie ready. Made with React, Javascript, Bootstrap. Next steps will be finishing the form section to send SMS texts on submit and the readme.",
+    link: "https://polebarn.herokuapp.com/",
+    gitlink: "https://github.com/Aaron-Waguespack/polebarn"
+},
+{
+    id: 2,
     image:"assets/reviews.png",
     name: "Maze Mercantile - Reviews",
     desc: "Maze Mercantile an e-commerce shoe retailer. Part of a 3 person team connecting an existing React.JS front end to a database and deploying on AWS. Personally in charge of the reviews section of the website. Database selection and schema creation. Connecting server and database to existing frontend. Deploying on AWS EC2 micro service with multiple servers and a load balancer.",
@@ -8,7 +16,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/reviewsService"
 },
 {
-    id: 2,
+    id: 3,
     image:"assets/QnA1.png",
     name: "Maze Mercantile - Q&A",
     desc: "Maze Mercantile an e-commerce shoe retailer. Part of a 3 person team updating a client facing retail web portal. Personally in charge of creating a new Q&A module to show questions and their corresponding answers. When expanded it makes additional data scrollable without adding to the height of the website. Modals show pictures and forms and allows for the addition, updating and reporting of data as well as the ability to upload pictures. Search functionality filters and dynamically highlights searched text. Created with React, Node, Express, Bootstrap, Moment, AWS, Docker, Heroku.",
@@ -16,7 +24,7 @@ export const projects = [
     gitlink: "https://github.com/Maze-Merch/QnA"
 },
 {
-    id: 3,
+    id: 4,
     image:"assets/mvp1.png",
     name: "Game Picker",
     desc: "Using the Steam games API, download a user's game library. Matches library information with information from each game. Dynamically displays each game in a card that is filterable by genre. Created using React, Node, Express, Bootstrap, Heroku",
@@ -24,7 +32,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/game_picker"
 },
 {
-    id: 4,
+    id: 5,
     image:"assets/facerec.jpg",
     name: "Face Recognition",
     desc: "App that allows you to create a account and using a self learning Clarifai API it locates faces on a picture and puts a box around it. Built with HTML5, CCS3, Javascript, React for the front end, Node.js, NPM, Express.js for the back end. User names and (hashed) password are stored using Postgresql for the database. It was created on Sublime3 and deployed on Heroku.",
@@ -32,7 +40,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/facerecognition"
 },
 {
-    id: 5,
+    id: 6,
     image:"assets/yelpcamp.jpg",
     name: "Yelp Camp",
     desc: "YelpCamp is a yelp style website for campgrounds.Once you create an account you can add and edit campsites, as well as add comments on other campsites. It uses HTML5, CCS3, Javascript, Bootstrap 4, jQuery on the front end, Node.js, NPM, Express.js, REST, Passport.js, MongoDB. Developed on Cloud9 IDE and hosted on Heroku and MongoLab.",
@@ -40,7 +48,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/YelpCamp"
 },
 {
-    id: 6,
+    id: 7,
     image:"assets/RoboFriends.jpg",
     name: "RoboFriends",
     desc: "Using React this app dynamically creates cards for each robot from a url list. From there the search box removes unrelated cards on the click of each letter. Created with HTML5, CCS3, Javascript, React, React-Dom and tachyons.",
@@ -48,7 +56,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/robofriends"
 },
 {
-    id: 7,
+    id: 8,
     image:"assets/patatapclone.jpg",
     name: "Patatap Clone",
     desc: "Music creation app with sound and animations on keypress. Created with HTML5, CCS3, Javascript, using the jQuery, Howler JS and Paper JS libraries.",
@@ -56,7 +64,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/patatapclone01"
 },
 {
-    id: 8,
+    id: 9,
     image:"assets/rgbcolorgame.jpg",
     name: "RGB Color Game",
     desc: "Project to practice RGB color recognition and DOM manipulation. Created with HTML5, CCS3 and Javascript.",
@@ -64,7 +72,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/rgbColorGame"
 },
 {
-    id: 9,
+    id: 10,
     image:"assets/backgroundgen.jpg",
     name: "Background Generator",
     desc: "Project that allows you to pick colors from the color wheel then have them automatically applied to the background gradients. It also provides you the codes for the colors and a section to copy the code to apply it to other projects. Includes a randomize button that picks random colors and applies them to the background. Created with HTML5, CCS3 and Javascript.",
@@ -72,7 +80,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/backgroundgen"
 },
 {
-    id: 10,
+    id: 11,
     image:"assets/wedding.jpg",
     name: "Wedding Photos",
     desc: "Created a photo gallery which dynamically creates and expands the list of photos based on the total amount of photos in the directory. Contains onclicks, mouseover effects and a thumbnail gallery	whenever a photo is expanded. Created with HTML5, CCS3, Javascript, Bootstrap 3 & Lightbox",
@@ -80,7 +88,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/weddingpictures"
 },
 {
-    id: 11,
+    id: 12,
     image:"assets/todolist.jpg",
     name: "To Do List",
     desc: "A small to do list app that allows you add new items, remove items, cross off items. Created with HTML5, CCS3, Javascript and jQuery.",
@@ -88,7 +96,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/todolist"
 },
 {
-    id: 12,
+    id: 13,
     image:"assets/datingapp.jpg",
     name: "Dating App",
     desc: "Landing Page with navbar, buttons. Created with HTML5, CCS3, Javascript Bootstrap 3 and Google Fonts",
@@ -96,7 +104,7 @@ export const projects = [
     gitlink: "https://github.com/Aaron-Waguespack/datingapp"
 },
 {
-    id: 13,
+    id: 14,
     image:"assets/museumofcandy.jpg",
     name: "Museum of Candy",
     desc: "Responsive landing page made not to look like Bootstrap.Created with HTML5, CCS3, Javascript Bootstrap 4",
